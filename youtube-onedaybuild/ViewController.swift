6b2cb74e05e5f1632f39930f820e6ccef55bc3cf
Demetrice Sims <meech24@gmail.com>
2020-08-26 15:32:46 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Demetrice Sims on 8/26/20.
+//  Copyright © 2020 Demetrice Sims. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
