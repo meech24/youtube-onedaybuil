9a6187becaf9ef0ff7c5fcd42149de4c5ff3845d
Demetrice Sims <meech24@gmail.com>
2020-09-28 14:46:47 -0700

    added to test

---
+++ youtube-onedaybuild/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild
+//
+//  Created by Demetrice Sims on 8/31/20.
+//  Copyright © 2020 Demetrice Sims. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = "AIzaSyBUIf3CXDdDxU9geFNlaad17Kna757xASo"
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Demetrice Sims on 8/31/20.
+//  Copyright © 2020 Demetrice Sims. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
