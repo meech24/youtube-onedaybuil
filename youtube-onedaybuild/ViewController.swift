489413f3f08c36feca79a4a197af5f09cbaceaf3
Demetrice Sims <meech24@gmail.com>
2020-09-28 15:50:58 -0700

    added

--- youtube-onedaybuild/ViewController.swift
+++ youtube-onedaybuild/ViewController.swift
@@ -14,9 +14,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var i = 0
         
-        //model.getVideos()
+        
+        model.getVideos()
     }
 
 
