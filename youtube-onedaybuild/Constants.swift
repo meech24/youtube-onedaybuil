ce0025d1421712f4d14177d6f86f50c291a5b2cd
Demetrice Sims <meech24@gmail.com>
2020-10-23 23:24:19 -0700

    Added tableview, custom protocol in the model, displaying video titles in the tableview

--- youtube-onedaybuild/Constants.swift
+++ youtube-onedaybuild/Constants.swift
@@ -12,4 +12,5 @@ struct Constants {
     static var API_KEY = "AIzaSyBUIf3CXDdDxU9geFNlaad17Kna757xASo"
     static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell" 
 }
--- youtube-onedaybuild/Model.swift
+++ youtube-onedaybuild/Model.swift
@@ -8,8 +8,14 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    func videosFetched(_ videos: [Video])
+}
+
 
 class Model {
+    
+    var delegate: ModelDelegate?
     
     func getVideos() {
         //Create a URL object
@@ -34,6 +40,16 @@ class Model {
                            decoder.dateDecodingStrategy = .iso8601
                            
                           let response = try decoder.decode(Response.self, from: data!)
+                if response.items != nil{
+                    DispatchQueue.main.async {
+                        
+                        // call the "videoFetched" method of the delegate
+                                       
+                                           self.delegate?.videosFetched(response.items!)
+                        
+                    }
+               
+                    }
                 
                 dump(response)
             }
